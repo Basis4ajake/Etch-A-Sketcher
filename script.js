8eb8cd4025edfede0dfbf498b4d491accbf070c0
Jacob Marlowe <jacob.marlowe1991@gmail.com>
2024-10-12 01:01:25 -0400

    Create container with 16 grid squares

---
+++ script.js
@@ -0,0 +1,8 @@
+const container = document.querySelector(".container")
+
+for (i = 0; i < 16; i++) {
+    const gridSquare = document.createElement("div")
+    gridSquare.classToggle("gridSqaure")
+    
+    
+}
