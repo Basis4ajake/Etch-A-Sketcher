4dbe49d2a8ebc8116cbac8fe08326a0643ed8ba3
Jacob Marlowe <jacob.marlowe1991@gmail.com>
2024-10-12 14:54:13 -0400

    Change color on mouse over and add classes to each grid tile

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const container = document.querySelector(".container")
+let gridSquare
 let i = 0;
 do { 
-    const gridSquare = document.createElement("div");
-    gridSquare.classList.toggle("gridSquare");
+    gridSquare = document.createElement("div");
+    gridSquare.classList.add("gridSquare", `square-${i + 1}`, 'gridSquare:hover');
     container.appendChild(gridSquare);
     i++;
-} while (i < 16);
+} while (i < 256);
+
+gridSquare.addEventListener("onhover", () => {
+    gridSquare.setAttribute("style", "background-color: green;");
+    console.log(gridSquare.classList)
+});
+
+console.log(gridSquare)
