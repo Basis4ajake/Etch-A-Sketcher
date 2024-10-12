e07eeb6545817f29c950f4d75540521125124468
Jacob Marlowe <jacob.marlowe1991@gmail.com>
2024-10-12 01:24:03 -0400

    Add backround to square and border onto grids

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const container = document.querySelector(".container")
-
-for (i = 0; i < 16; i++) {
-    const gridSquare = document.createElement("div")
-    gridSquare.classToggle("gridSqaure")
-    
-    
-}
+let i = 0;
+do { 
+    const gridSquare = document.createElement("div");
+    gridSquare.classList.toggle("gridSquare");
+    container.appendChild(gridSquare);
+    i++;
+} while (i < 16);
