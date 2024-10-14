14568ad6b1c263ceeb596ead3ab7136a5542a537
Jacob Marlowe <jacob.marlowe1991@gmail.com>
2024-10-14 00:04:31 -0400

    Add mouse eventlisteners to squares during the square creation

--- script.js
+++ script.js
@@ -4,13 +4,24 @@ let i = 0;
 do { 
     gridSquare = document.createElement("div");
     gridSquare.classList.add("gridSquare", `square-${i + 1}`, 'gridSquare:hover');
+    gridSquare.addEventListener("mouseover", e => {
+        e.target.classList.toggle('mousedInto')
+        console.log(e);
+    })
     container.appendChild(gridSquare);
     i++;
 } while (i < 256);
 
-gridSquare.addEventListener("onhover", () => {
-    gridSquare.setAttribute("style", "background-color: green;");
-    console.log(gridSquare.classList)
+const gridSquares = document.querySelectorAll(".gridsSquare")
+gridSquares.forEach(gridSquare => {
+        gridSquare.addEventListener("mouseover", e => {
+        e.target.classList.toggle('mousedInto');
+        console.log(e);
+    })
 });
 
-console.log(gridSquare)
+// addEventListener("mouseover", e => {
+//     e.target.classList.toggle('mousedInto');
+//     console.log(e)
+// });
+
