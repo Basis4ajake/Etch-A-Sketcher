0b28f2fd925bf286ba23b5a18333cdf55c3e600b
Jacob Marlowe <jacob.marlowe1991@gmail.com>
2024-10-14 00:36:15 -0400

    Solve how to change colors while moving mouse
    Solved both methods for changing color, which
    should be useful if I get stuck at a later step
    and need to change how I'm approaching the next
    problem as I go. Implemented solution 1 of changing
    color by toggling class as the mouse moves over
    eachgrid tile. Solution 2 is changing color during
    mouseover event by changing the style of the event
    target.

--- script.js
+++ script.js
@@ -5,20 +5,28 @@ do {
     gridSquare = document.createElement("div");
     gridSquare.classList.add("gridSquare", `square-${i + 1}`, 'gridSquare:hover');
     gridSquare.addEventListener("mouseover", e => {
-        e.target.classList.toggle('mousedInto')
+        // e.target.classList.toggle('mousedInto')
         console.log(e);
+        console.log(e.target);
+        e.target.style.backgroundColor = 'red';
     })
     container.appendChild(gridSquare);
     i++;
 } while (i < 256);
 
+console.dir(gridSquare)
+
 const gridSquares = document.querySelectorAll(".gridsSquare")
-gridSquares.forEach(gridSquare => {
-        gridSquare.addEventListener("mouseover", e => {
-        e.target.classList.toggle('mousedInto');
-        console.log(e);
-    })
-});
+console.log(Array.isArray(gridSquares))
+const squareArray = [...gridSquares]
+console.log(Array.isArray(squareArray))
+
+// squareArray.forEach((gridSquare) => {
+//         gridSquare.addEventListener("mouseover", e => {
+//         e.target.classList.toggle('mousedInto');
+//         console.log(e);
+//     })
+// });
 
 // addEventListener("mouseover", e => {
 //     e.target.classList.toggle('mousedInto');
