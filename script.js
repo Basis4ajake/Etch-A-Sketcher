6ff3f4638e8695606d0c8aaf45b60d4069e4e7cd
Jacob Marlowe <jacob.marlowe1991@gmail.com>
2024-10-15 12:57:17 -0400

    Setup color list for color changing backgrounds

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 const container = document.querySelector(".container")
 let gridSquare
+let setGridNumber
+
+function getGridNumber() {
+    setGridNumber = prompt('How many squares should the grid sides be? Enter a number less than 100');
+    console.log(setGridNumber);
+    console.dir(setGridNumber);
+    return gridArea = setGridNumber * setGridNumber;
+}
+
+const colors = ["red", "green", "blue", "yellow", "teal", "black", "orange", "purple", "goldenrod", "maroon", "aquamarine", "brown", 
+"indigo", "violet"];
+
+function 
+// function checkGridNumber() {
+//     while ((setGridNumber > 100) || (setGridNumber != Number)) {
+
+//          setGridNumber = prompt(`You entered ${setGridNumber}. Make sure to type a number less than 100 and type in as a number and not spelled out using letters.`);
+//      return
+// }
+
+let gridNumber = getGridNumber();
+
 let i = 0;
 do { 
     gridSquare = document.createElement("div");
@@ -8,11 +30,16 @@ do {
         // e.target.classList.toggle('mousedInto')
         console.log(e);
         console.log(e.target);
+        console.dir(e)
+        console.dir(e.type)
+        console.dir(e.target)
+        console.dir(e.timeStamp)
+        console.dir(e)
         e.target.style.backgroundColor = 'red';
     })
     container.appendChild(gridSquare);
     i++;
-} while (i < 256);
+} while (i < gridNumber);
 
 console.dir(gridSquare)
 
