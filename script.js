d1c6f965f13a4e1bd6fe547c37a0b48df0a7712e
Jacob Marlowe <jacob.marlowe1991@gmail.com>
2024-10-16 14:52:51 -0400

    Setup reset feature

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const container = document.querySelector(".container")
 let gridSquare
 let gridRow
+let setGridNumber
 
 function getGridNumber() {
-    let setGridNumber = prompt('How many squares should the grid sides be? Enter a number less than 100');
+     setGridNumber = prompt('How many squares should the grid sides be? Enter a number less than 100');
     if (setGridNumber > 100) {
         checkGridNumber()
     }
@@ -37,80 +38,49 @@ function getRandomHexColor(){
     return randomHexColor = "#" + hexDigitOne +  hexDigitTwo + hexDigitThree + hexDigitFour + hexDigitFive + hexDigitSix + "";
 }
 
-let gridNumber = getGridNumber();
-
-for (let i = 0; i < gridNumber; i++) {        //Loop to create Grid Rows and attach to Container. Number of Grid rows determined by user provided grid number
-    gridRow = document.createElement("div");
-    gridRow.classList.add("gridRow");
-    gridRow.setAttribute("style", "display: flex; flex: 1; flex-flow: wrap;")
-    container.appendChild(gridRow);
-    for (let i = 0; i < gridNumber; i++) {     //Nested loop to create grid squares and attach group of grid squares to each grid Row. 
-        gridSquare = document.createElement("div");    //Number of grid squares per row also determined by user provided grid number.
-        gridSquare.classList.add("gridSquare", `square-${i + 1}`, 'gridSquare:hover');
-        gridSquare.addEventListener("mouseover", e => {
-        console.log(e);       
-        console.log(e.target);
-        console.dir(e)
-        console.dir(e.type)
-        console.dir(e.target)
-        console.dir(e.timeStamp)
-        console.dir(e)
-        e.target.style.backgroundColor = getRandomHexColor();
-            })
-    gridRow.appendChild(gridSquare);
-    }; 
-};
-
-
-// do { 
-//     gridSquare = document.createElement("div");
-//     gridSquare.classList.add("gridSquare", `square-${i + 1}`, 'gridSquare:hover');
-//         gridSquare.addEventListener("mouseover", e => {
-//         // e.target.classList.toggle('mousedInto')
-//         console.log(e);
-//         console.log(e.target);
-//         console.dir(e)
-//         console.dir(e.type)
-//         console.dir(e.target)
-//         console.dir(e.timeStamp)
-//         console.dir(e)
-//         e.target.style.backgroundColor = getRandomHexColor();
-//     })
-//     container.appendChild(gridSquare);
-//     i++;
-// } while (i < gridNumber);
-
-console.dir(gridSquare)
-
-function createGridRows(){
+function createGrid() {
+    if (document.querySelector(".gridRow")){
+        deleteGrid();
+    }
     let gridNumber = getGridNumber();
-    for (i = 0; i < gridNumber; i++) {
+    for (let i = 0; i < gridNumber; i++) {        //Loop to create Grid Rows and attach to Container. Number of Grid rows determined by user provided grid number
         gridRow = document.createElement("div");
         gridRow.classList.add("gridRow");
+        gridRow.setAttribute("style", "display: flex; flex: 1; flex-flow: wrap;")
         container.appendChild(gridRow);
-    }
-}
+        for (let i = 0; i < gridNumber; i++) {     //Nested loop to create grid squares and attach group of grid squares to each grid Row. 
+            gridSquare = document.createElement("div");    //Number of grid squares per row also determined by user provided grid number.
+            gridSquare.classList.add("gridSquare", `square-${i + 1}`, 'gridSquare:hover');
+            gridSquare.addEventListener("mouseover", e => {
+            console.log(e);       
+            console.log(e.target);
+            console.dir(e)
+            console.dir(e.type)
+            console.dir(e.target)
+            console.dir(e.timeStamp)
+            console.dir(e)
+            e.target.style.backgroundColor = getRandomHexColor();
+            })
+        gridRow.appendChild(gridSquare);
+        }; 
+    };
+};
 
-function createSquaresInRows(){
-    let gridRows = document.querySelectorAll(gridRow)
-    for (row of gridRows) {
-        let i = 0;
-        do {
-        gridSquare = document.createElement("div");
-        gridSquare.classList.add("gridSquare", `square-${i + 1}`, 'gridSquare:hover');
-    
-        gridSquare.addEventListener("mouseover", e => {
-        // e.target.classList.toggle('mousedInto')
-        console.log(e);
-        console.log(e.target);
-        console.dir(e)
-        console.dir(e.type)
-        console.dir(e.target)
-        e.target.style.backgroundColor = getRandomHexColor();
+function deleteGrid(){
+    let gridRows = document.querySelectorAll(".gridRow");
+    gridRows.forEach(gridRow => {
+        gridRow.remove();
     })
-    gridRow.appendChild(gridSquare)
-    // container.appendChild(gridSquare);
-    i++;
-    } while (i < gridNumber);
-};
-};
+    };
+
+const resetTool = document.createElement("div"); //create container for reset button
+resetTool.classList.add("resetTool");
+document.body.prepend(resetTool);
+
+const resetButton = document.createElement("button"); //Create reset button and add to container
+resetButton.classList.add("resetButton");
+resetButton.textContent = "Click here to reset and create a new grid"
+resetButton.addEventListener("click", createGrid)
+resetTool.append(resetButton)
+
+// createGrid();
