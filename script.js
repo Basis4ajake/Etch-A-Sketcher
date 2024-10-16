ee12f03c00cb9f69d92504c64755dcc295d5d857
Jacob Marlowe <jacob.marlowe1991@gmail.com>
2024-10-15 22:10:28 -0400

    Create row system to group squares inside
    Created a row system so that the user inputted number
    creates user entered number of rows, then generates
    the same number of squares within each row. Used
    a nested loop to accomplish this, loop 1 creates
    a row, then loop 2 creates the required number
    of squares for the row. The process continues until
    the number of rows and squares created equals
    the user input.

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 const container = document.querySelector(".container")
 let gridSquare
-let setGridNumber
+let gridRow
 
 function getGridNumber() {
-    setGridNumber = prompt('How many squares should the grid sides be? Enter a number less than 100');
+    let setGridNumber = prompt('How many squares should the grid sides be? Enter a number less than 100');
+    if (setGridNumber > 100) {
+        checkGridNumber()
+    }
     console.log(setGridNumber);
     console.dir(setGridNumber);
     return setGridNumber;
 }
 
-const colors = ["red", "green", "blue", "yellow", "teal", "black", "orange", "purple", "goldenrod", "maroon", "aquamarine", "brown", 
-"indigo", "violet"];
+function checkGridNumber(){
+    do {
+        setGridNumber = prompt(`You entered either a word or a number higher than 100. You entered ${setGridNumber}. Please try again. Enter a number less than 100 to set the number of squares per side`);
 
+    } while (setGridNumber > 100);
+}
+
+/* Select a random Hex Color */
 const hexColorCharacters = ["A", "B", "C", "D", "E", "F", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 function getHexCharacters(){
@@ -29,23 +37,18 @@ function getRandomHexColor(){
     return randomHexColor = "#" + hexDigitOne +  hexDigitTwo + hexDigitThree + hexDigitFour + hexDigitFive + hexDigitSix + "";
 }
 
-function getColor() {
-    let randomColorChoice = Math.floor(Math.random() * (colors.length - 1));
-    console.log(colors[randomColorChoice]);
-    return colors[randomColorChoice];
-};
-
 let gridNumber = getGridNumber();
-let i = 0;
-do { 
+
+for (let i = 0; i < gridNumber; i++) {        //Loop to create Grid Rows and attach to Container. Number of Grid rows determined by user provided grid number
     gridRow = document.createElement("div");
     gridRow.classList.add("gridRow");
-
-    gridSquare = document.createElement("div");
-    gridSquare.classList.add("gridSquare", `square-${i + 1}`, 'gridSquare:hover');
-    gridSquare.addEventListener("mouseover", e => {
-        // e.target.classList.toggle('mousedInto')
-        console.log(e);
+    gridRow.setAttribute("style", "display: flex; flex: 1; flex-flow: wrap;")
+    container.appendChild(gridRow);
+    for (let i = 0; i < gridNumber; i++) {     //Nested loop to create grid squares and attach group of grid squares to each grid Row. 
+        gridSquare = document.createElement("div");    //Number of grid squares per row also determined by user provided grid number.
+        gridSquare.classList.add("gridSquare", `square-${i + 1}`, 'gridSquare:hover');
+        gridSquare.addEventListener("mouseover", e => {
+        console.log(e);       
         console.log(e.target);
         console.dir(e)
         console.dir(e.type)
@@ -53,11 +56,61 @@ do {
         console.dir(e.timeStamp)
         console.dir(e)
         e.target.style.backgroundColor = getRandomHexColor();
-    })
-    container.appendChild(gridSquare);
-    i++;
-} while (i < gridNumber);
+            })
+    gridRow.appendChild(gridSquare);
+    }; 
+};
+
+
+// do { 
+//     gridSquare = document.createElement("div");
+//     gridSquare.classList.add("gridSquare", `square-${i + 1}`, 'gridSquare:hover');
+//         gridSquare.addEventListener("mouseover", e => {
+//         // e.target.classList.toggle('mousedInto')
+//         console.log(e);
+//         console.log(e.target);
+//         console.dir(e)
+//         console.dir(e.type)
+//         console.dir(e.target)
+//         console.dir(e.timeStamp)
+//         console.dir(e)
+//         e.target.style.backgroundColor = getRandomHexColor();
+//     })
+//     container.appendChild(gridSquare);
+//     i++;
+// } while (i < gridNumber);
 
 console.dir(gridSquare)
 
+function createGridRows(){
+    let gridNumber = getGridNumber();
+    for (i = 0; i < gridNumber; i++) {
+        gridRow = document.createElement("div");
+        gridRow.classList.add("gridRow");
+        container.appendChild(gridRow);
+    }
+}
 
+function createSquaresInRows(){
+    let gridRows = document.querySelectorAll(gridRow)
+    for (row of gridRows) {
+        let i = 0;
+        do {
+        gridSquare = document.createElement("div");
+        gridSquare.classList.add("gridSquare", `square-${i + 1}`, 'gridSquare:hover');
+    
+        gridSquare.addEventListener("mouseover", e => {
+        // e.target.classList.toggle('mousedInto')
+        console.log(e);
+        console.log(e.target);
+        console.dir(e)
+        console.dir(e.type)
+        console.dir(e.target)
+        e.target.style.backgroundColor = getRandomHexColor();
+    })
+    gridRow.appendChild(gridSquare)
+    // container.appendChild(gridSquare);
+    i++;
+    } while (i < gridNumber);
+};
+};
